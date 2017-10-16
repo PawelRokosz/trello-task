dd9a10654dffebb9db81be8a8aa26ecf629b3b01
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-10-16 17:33:41 +0200

    tasks from data added to state

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 import List from './List';
+import tasks from './data/data.json';
 
 class Lists extends Component {
+
+  state = {
+    tasks: []
+  }
+
+  componentWillMount() {
+    this.setState({
+      tasks
+    })
+  }
 
   render() {
     return (
